b2f7762541eeeaee59212d7277f002a479cce2ce
Nicolás Della Penna <nikete@gmail.com>
2021-05-06 01:41:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('todos')
+block_ref = db.collection('blocks')
 
 @app.route('/bid', methods=['POST'])
 def create():
@@ -22,29 +22,20 @@ def create():
     """
     try:
         id = request.json['block']
-        todo_ref.document(id).set(request.json)
+        block_ref.document(id).set(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
-@app.route('/list', methods=['GET'])
+@app.route('/winner', methods=['GET'])
 def read():
     """
         read() : lists all bids in a given block as JSON.
-        all_todos : Return all documents.
     """
-    try:
-        # Check if ID was passed to URL query
-        todo_id = request.args.get('block')
-        if todo_id:
-            todo = todo_ref.document(todo_id).get()
-            return jsonify(todo.to_dict()), 200
-        else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-            return jsonify(all_todos), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
+    block_id = request.args.get('block')
+    if block_id:
+        block = block_ref.document(block_id).get()
+        return jsonify(block.to_dict()), 200
 
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':

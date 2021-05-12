0deefec0ef06f8d422378a12e442680195c2ca3d
Nicolás Della Penna <nikete@gmail.com>
2021-05-12 17:40:02 +0200

    Create app.py

--- app.py
+++ app.py
@@ -3,73 +3,57 @@
 import os
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore, initialize_app
-
+from time import time
 # Initialize Flask app
 app = Flask(__name__)
 
 # Initialize Firestore DB
-# cred = credentials.Certificate('key.json')
-default_app = initialize_app()
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('todos')
 
-@app.route('/add', methods=['POST'])
-def create():
+bids_ref = db.collection('bids')
+bids_ref = db.collection('txs')
+
+@app.route('/bid', methods=['POST'])
+def bid():
     """
-        create() : Add document to Firestore collection with request body.
-        Ensure you pass a custom ID as part of json body in post request,
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
+        bid() : Add bid to Firestore collection with request body.
     """
     try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
+        id = hash(request.json['bundle'] + request.json['bidder'])
+        bids_ref.document(id).set(request.json)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
+@app.route('/close', methods=['GET'])
+def close():
+    """
+        dispatch any closed ones that havent been dispatched yet (so hit this often).
+    """
+    for tx in db.collection(u'tx').where(u'timestamp', u'<', time() -30 ).where(u'Auction',u'==',u'open').stream():
+        highest_bid,highest_bundle = 0,[]
+        for bids in db.collection(u'bids').where(u'tx',u'==',tx.tx ).stream():
+               #calculate the value for the user of the bid
+                bid_value = 0
+                if bid_value > highest_bid:
+                    highest_bid,highest_bundle=bid_value,bundle
+        # Udpdate auction for the tx to closed:
+        tx.update({"Auction": "closed", "Winner_Bundle": highest_bundle, Winner_bid:highest_bid})
+ 
 @app.route('/list', methods=['GET'])
-def read():
-    """
-        read() : Fetches documents from Firestore collection as JSON.
-        todo : Return document that matches query ID.
-        all_todos : Return all documents.
-    """
+def list():
+    #    db.collection(u'bids').where(u'time', u'>', request.json['tx'] ).stream()
     try:
         # Check if ID was passed to URL query
-        todo_id = request.args.get('id')
-        if todo_id:
+        tx = request.args.get('tx')
+        if tx:
             todo = todo_ref.document(todo_id).get()
             return jsonify(todo.to_dict()), 200
         else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            all_open = [doc.to_dict() for doc in todo_ref.stream()]
             return jsonify(all_todos), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
-    """
-        update() : Update document in Firestore collection with request body.
-        Ensure you pass a custom ID as part of json body in post request,
-        e.g. json={'id': '1', 'title': 'Write a blog post today'}
-    """
-    try:
-        id = request.json['id']
-        todo_ref.document(id).update(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
-
-@app.route('/delete', methods=['GET', 'DELETE'])
-def delete():
-    """
-        delete() : Delete a document from Firestore collection.
-    """
-    try:
-        # Check for ID in URL query
-        todo_id = request.args.get('id')
-        todo_ref.document(todo_id).delete()
-        return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occured: {e}"
 
